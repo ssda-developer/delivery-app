e444287fe8c66b55f5c4377f9619257fbafdf1f1
Sergio SIRITSA <ssda.blablaman@gmail.com>
2022-06-26 17:51:00 +0300

    chore: removed unused files

--- src/context/ShopsContext.js
+++
@@ -1,7 +0,0 @@
-// import { createContext } from 'react';
-//
-// const ShopsContext = createContext({});
-//
-// export const ShopsProvider = ShopsContext.Provider;
-//
-// export default ShopsContext;
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import { StoreProvider } from './context/StoreContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
     <React.StrictMode>
         <BrowserRouter>
@@ -16,7 +17,4 @@ root.render(
     </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
